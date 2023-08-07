6c0c934b4d6ca94894881dbec556c991eff305ea
Bordoloi <Arnab.Bordoloi2@shell.com>
2023-08-07 14:26:15 -0500

    part0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p> hello {props.name}, you are {props.age} years old</p>
     </div>
-  );
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/Arnab-Bordoloi'>arnab</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  const friends = [
+    { name: 'Peter', age: 4},
+    { name: 'Maya', age: 10},
+  ]
+  const friend2 = ['John', 'Bob']
+
+  return (
+    <div>
+      <h1> Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Hello />
+      <Hello />
+      <Footer />
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+      <p>{friend2}</p>
+    </div>
+  )
 }
 
 export default App;
